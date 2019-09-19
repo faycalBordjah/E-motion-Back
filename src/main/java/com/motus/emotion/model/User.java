12e84378bbb052c8076e66aaa1c3f1948e03fb5a
Fayçal BORDJAH <fbordjah@linagora.com>
2019-09-19 11:51:21 +0200

    Configure mysql server and create user entiry

---
+++ src/main/java/com/motus/emotion/model/User.java
@@ -0,0 +1,156 @@
+package com.motus.emotion.model;
+
+import com.motus.emotion.model.custom.Address;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private Date birthDay;
+    @Column
+    private String mail;
+    @Column
+    private String password;
+    @Column
+    private int zipCode;
+    @Column
+    private int permitNum;
+    @Column
+    @Embedded
+    private Address address;
+    @Column(nullable = false, updatable = false)
+    private Date creationDate;
+    @Column(nullable = false)
+    @LastModifiedDate
+    private Date modificationDate;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, Date birthDay, String mail, String password, int permitNum, Address address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDay = birthDay;
+        this.mail = mail;
+        this.password = password;
+        this.permitNum = permitNum;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPermitNum() {
+        return permitNum;
+    }
+
+    public void setPermitNum(int permitNum) {
+        this.permitNum = permitNum;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDay=" + birthDay +
+                ", mail='" + mail + '\'' +
+                ", password='" + password + '\'' +
+                ", permitNum=" + permitNum +
+                ", address=" + address +
+                '}';
+    }
+}
---
+++ src/main/java/com/motus/emotion/model/custom/Address.java
@@ -0,0 +1,87 @@
+package com.motus.emotion.model.custom;
+
+public class Address {
+
+    private int number;
+
+    private String street;
+
+    private String city;
+
+    private String state;
+
+    private String zip;
+
+    private String country;
+
+    public Address() { }
+
+    public Address(int number, String street, String city, String state, String zip, String country) {
+        this.number = number;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.country = country;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "number=" + number +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
