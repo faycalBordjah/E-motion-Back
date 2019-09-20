8a65e055d0757ace60e08ebed3c774e313ce4f35
Fayçal BORDJAH <fbordjah@linagora.com>
2019-09-20 16:07:06 +0200

    Change the sql dialect version to v8 and add optional return

--- src/main/java/com/motus/emotion/service/UserService.java
+++ src/main/java/com/motus/emotion/service/UserService.java
@@ -6,14 +6,20 @@ import com.motus.emotion.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
-    public User getUser(Long id) throws NotFoundException {
-        return userRepository.findById(id).orElseThrow(() -> new NotFoundException("User not found"));
+    public Optional <User> getUser(Long id) throws NotFoundException {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()){
+            throw new NotFoundException("dddd");
+        }
+        return userRepository.findById(id);
     }
 
     public User addUser(User user){
