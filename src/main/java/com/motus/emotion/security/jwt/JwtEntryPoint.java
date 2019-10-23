085082d8911f65de85934324895ebc5fd4283eeb
Fayçal BORDJAH <fbordjah@linagora.com>
2019-10-23 13:00:51 +0200

    Add a better exception to handle bad crede,tials

--- src/main/java/com/motus/emotion/security/jwt/JwtEntryPoint.java
+++ src/main/java/com/motus/emotion/security/jwt/JwtEntryPoint.java
@@ -1,7 +1,11 @@
 package com.motus.emotion.security.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.Authenticator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -19,7 +23,10 @@ public class JwtEntryPoint implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest httpServletRequest,
                          HttpServletResponse httpServletResponse,
                          AuthenticationException e) throws IOException, ServletException {
-        LOGGER.warn("Jwt authentication failed ", e);
-        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Jwt authentication failed");
+        LOGGER.warn("Unauthorized  {}", e.getMessage());
+        if (e instanceof BadCredentialsException){
+            httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "You have put bad credentials ");
+        }else {httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Jwt authentication failed");}
     }
 }
